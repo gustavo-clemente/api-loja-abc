81a33a7b2947ada5c6094d5b8326f1c14aa45e42
Gustavo Clemente <gustavo.lopes@grupomaisvalor.com;br>
2024-02-29 19:46:30 -0300

    test: Add unit test for ProductService

---
+++ tests/Unit/Domain/Sales/Service/ProductServiceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace Tests\Unit\Domain\Sales\Service;
+
+use App\Domain\Sales\Entity\Product;
+use App\Domain\Sales\Repository\ProductRepository;
+use App\Domain\Sales\Service\ProductService;
+use App\Domain\Sales\ValueObject\ProductId;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class ProductServiceTest extends TestCase
+{
+    public function testGetAllProductsReturnsProductArray(): void
+    {
+        $this->mock(ProductRepository::class, function (MockInterface $mock): void {
+            $mock
+              ->shouldReceive("findAll")
+              ->once()
+              ->andReturn([
+                new Product(
+                    id: new ProductId("1"),
+                    name: "Product 1",
+                    price: 2000,
+                    description: ""
+                ),
+                new Product(
+                    id: new ProductId("2"),
+                    name: "Product 2",
+                    price: 2000,
+                    description: ""
+                ),
+                new Product(
+                    id: new ProductId("3"),
+                    name: "Product 3",
+                    price: 2000,
+                    description: ""
+                )
+              ]);
+        });
+
+        $products = app(ProductService::class)->getAllProducts();
+
+        $this->assertIsArray($products);
+        $this->assertContainsOnlyInstancesOf(Product::class, $products);
+        $this->assertCount(3, $products);
+    }
+}
