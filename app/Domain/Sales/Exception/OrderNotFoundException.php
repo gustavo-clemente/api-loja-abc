6bdaeb71506a7c9c73cc3fbcdf4bf20125d1978b
Gustavo Clemente <gustavo.lopes@grupomaisvalor.com;br>
2024-03-03 19:41:07 -0300

    refactor: Remove code smell pointed on Sonar scan

--- app/Domain/Sales/Exception/OrderNotFoundException.php
+++ app/Domain/Sales/Exception/OrderNotFoundException.php
@@ -6,4 +6,8 @@ namespace App\Domain\Sales\Exception;
 
 class OrderNotFoundException extends SalesException
 {
+    public function __construct($message = "Order ID not found", $code = 404)
+    {
+        parent::__construct($message, $code);
+    }
 }
