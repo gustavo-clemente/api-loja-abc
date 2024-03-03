da2527fbc3d7352df5718352649fad3bb097fbc4
Gustavo Clemente <gustavo.lopes@grupomaisvalor.com;br>
2024-03-03 18:24:08 -0300

    doc: Add README.md
    Add a README.md file with installation, execution and documentation
    instructions.

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::resource("product", ProductController::class)->only("index");
 Route::resource("order", OrderController::class)->only("index", "store", "show", "destroy", "update");
